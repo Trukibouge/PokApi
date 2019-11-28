88a789b973bafde01c9da22d2079afb0f703b4d4
Trukibouge <trukibouge@gmail.com>
2019-11-28 17:42:37 +0100

    Pokedex Controller

---
+++ src/Pokedex/PokedexDTO.ts
@@ -0,0 +1,3 @@
+export interface PokedexCreationDTO{
+    name: string;
+}
--- src/Pokedex/pokedex.controller.ts
+++ src/Pokedex/pokedex.controller.ts
@@ -1,6 +1,30 @@
-import {Controller} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post} from "@nestjs/common";
+import {PokedexService} from "./pokedex.service";
+import {Pokedex} from "./Pokedex";
+import {PokedexCreationDTO} from "./PokedexDTO";
 
-@Controller()
+@Controller('pokedexes')
 export class PokedexController{
+    constructor(private readonly pokeRepo: PokedexService){}
 
+    @Get()
+    getAll(): Promise<Pokedex[]> {
+        return this.pokeRepo.getAllPokedex();
+    }
+
+    @Post()
+    savePokedex(@Body() pokeCreationDto: PokedexCreationDTO): Promise<void>{
+        const newPokedex = new Pokedex(pokeCreationDto.name);
+        return this.pokeRepo.savePokedex(newPokedex);
+    }
+
+    @Get(':id')
+    findById(@Param('id') id:string): Promise<Pokedex>{
+        return this.pokeRepo.findById(id);
+    }
+
+    @Post(':id')
+    removePokedex(@Param('id') id:string): Promise<void>{
+        return this.pokeRepo.removePokedex(id);
+    }
 }
