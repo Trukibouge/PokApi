e5d502c83f2bb79002e80c31503f9bef0e3fb8c4
Trukibouge <trukibouge@gmail.com>
2019-11-28 13:11:46 +0100

    Pokedex service

--- src/Pokedex/Pokedex.ts
+++ src/Pokedex/Pokedex.ts
@@ -1,29 +1,12 @@
 import {randomBytes} from "crypto";
 import { Pokemon } from "./Pokemon/Pokemon";
 
-const fetch = require("node-fetch");
-const pokedexJsonUrl = 'https://raw.githubusercontent.com/Trukibouge/pokemon.json/master/pokedex.json';
-
 export class Pokedex{
     public id = randomBytes(16).toString('hex');
-
-    private pokemonList;
     private discoveredPokemon = new Map<number, Pokemon>();
 
     constructor(public readonly name: string){
-        this.pokemonList = this.initializePokemonList();
-    }
-
-    async initializePokemonList(): Promise<Map<number, Pokemon>>{
-        let pokeListRaw = await fetch(pokedexJsonUrl).json();
-        let pokeList = new Map<number, Pokemon>();
-        pokeListRaw().forEach((pokemon) => this.pokemonList.set(pokemon.id, pokemon));
-        if(!pokeList){
-            return Promise.reject('Pokedex data fetch failed');
-        }
-        else{
-            return Promise.resolve(pokeList);
-        }
+        // this.pokemonList = this.initializePokemonList();
     }
 
     addDiscoveredPokemon(pokemon: Pokemon): void{
--- src/Pokedex/pokedex.service.ts
+++ src/Pokedex/pokedex.service.ts
@@ -2,9 +2,59 @@ import {Injectable} from "@nestjs/common";
 import {Pokemon} from "./Pokemon/Pokemon";
 import {Pokedex} from "./Pokedex";
 
+const fetch = require("node-fetch");
+const pokedexJsonUrl = 'https://raw.githubusercontent.com/Trukibouge/pokemon.json/master/pokedex.json';
+
 @Injectable()
 export class PokedexService{
-    private discoveredPokemon: Map<number, Pokedex>;
+
+    private pokemonList: Map<number, Pokemon>;
+    private pokedexMap = new Map<string, Pokedex>();
+
+    constructor(){
+        this.initializePokemonList().then();
+    }
+
+    savePokedex(pokedex: Pokedex): Promise<void>{
+        this.pokedexMap.set(pokedex.id, pokedex);
+        return Promise.resolve();
+    }
+
+    getAllPokedex(): Promise<Pokedex[]>{
+        return Promise.resolve(Array.from(this.pokedexMap.values()));
+    }
+
+    findById(id: string): Promise<Pokedex> {
+        const pokedex = this.pokedexMap.get(id);
+        if (!pokedex) {
+            return Promise.reject('Pokedex with id ${id} not found');
+        }
+        return Promise.resolve(pokedex);
+    }
+
+    removePokedex(id: string): Promise<void>{
+        const pokedex = this.pokedexMap.get(id);
+        if(!pokedex){
+            return Promise.reject('Pokedex with id ${id} not found');
+        }
+        else{
+            this.pokedexMap.delete(id);
+            return Promise.resolve();
+        }
+
+    }
+
+    async initializePokemonList(): Promise<Map<number, Pokemon>>{
+        let pokeListRaw = await fetch(pokedexJsonUrl).json();
+        let pokeList = new Map<number, Pokemon>();
+        pokeListRaw().forEach((pokemon) => this.pokemonList.set(pokemon.id, pokemon));
+        if(!pokeList){
+            return Promise.reject('Pokedex data fetch failed');
+        }
+        else{
+            return Promise.resolve(pokeList);
+        }
+    }
 
     // async discoverPokemon(pokemonId: number){
     //     let pokemonList : Pokemon[];
