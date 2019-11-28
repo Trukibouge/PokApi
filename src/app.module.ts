51971aafe69a56a0e32d565da5ff16527a7283d7
Trukibouge <trukibouge@gmail.com>
2019-11-28 18:36:13 +0100

    Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {PokemonController} from "./Pokedex/Pokemon/pokemon.controller";
+import {PokedexController} from "./Pokedex/pokedex.controller";
+import {PokedexService} from "./Pokedex/pokedex.service";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PokemonController, PokedexController],
+  providers: [AppService, PokedexService],
 })
 export class AppModule {}
